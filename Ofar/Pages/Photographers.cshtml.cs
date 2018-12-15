351937d73d29ea64ada7d4ebe1b12a80e88355e2
Ari Johannesson <arividar@gmail.com>
2018-12-15 11:36:47 +0000

    Adds Photographer list to web site

--- Ofar/Pages/Photographers.cshtml.cs
+++ Ofar/Pages/Photographers.cshtml.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ofar.Pages
 {
-    public class BlehModel : PageModel
+    public class PhotographersModel : PageModel
     {
+        public IEnumerable<string> Photographers { get; set; }
+
         public void OnGet()
         {
+            ViewData["Title"] = "Ofar Photography - Photographers";
+            Photographers = new[] 
+            { 
+                "Mary Ellen Mark", "Cindy Sherman", "Robert Frank", "Eliot Porter",
+                "Imogen Cunningham", "Frans Lanting", "Paul Strand", "Weegee",  
+                "Richard Avedon", "Don McCullin", "Edward Western", "Man Ray", 
+                "David Bailey",  "David LaChapelle", "Anne Geddes", "Dorothea Lange", 
+                "Robert Doisneau", "Steve McCurry", "Mario Testino", "André Kertész", 
+                "Andreas Gursky", "Robert Mapplethorpe", "Henri-Cartier Bresson", 
+                "Annie Leibovitz", "Sebastião Salgado", "Brassaï", "Ansel Adams", 
+                "Robert Capa" 
+            };
         }
     }
 }
