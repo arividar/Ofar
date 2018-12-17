73a4e7063770402d7b37ce84e7d7d830a3245a31
Ari Johannesson <arividar@gmail.com>
2018-12-17 23:54:59 +0000

    Sorts photorgrapher's list and adds some younger photographers

--- Ofar/Pages/Photographers.cshtml.cs
+++ Ofar/Pages/Photographers.cshtml.cs
@@ -1,26 +1,43 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ofar.Pages
 {
     public class PhotographersModel : PageModel
     {
-        public IEnumerable<string> Photographers { get; set; }
+        private readonly string[] photographers =
+        {
+                "Mary Ellen Mark", "Cindy Sherman", "Robert Frank", "Eliot Porter",
+                "Imogen Cunningham", "Frans Lanting", "Paul Strand", "Weegee",
+                "Richard Avedon", "Don McCullin", "Edward Western", "Man Ray",
+                "David Bailey",  "David LaChapelle", "Anne Geddes", "Dorothea Lange",
+                "Robert Doisneau", "Steve McCurry", "Mario Testino", "André Kertész",
+                "Andreas Gursky", "Robert Mapplethorpe", "Henri-Cartier Bresson",
+                "Annie Leibovitz", "Sebastião Salgado", "Brassaï", "Ansel Adams",
+                "Robert Capa", "Camila Falquez", "Silvia Grav", "David Uzochukwu",
+                "Alecsandra Raluca Dragoi", "Nadine Ijewere", "Nicholas Scarpinato",
+                "Zev Hoover", "Oliver Charles", "Rachel Baran", "Lee Jeffries",
+                "Katharina Jung", "Alex Stoddard", "Ailera Stone", "Evan Atwood",
+                "Diane Sagnier", "Alex Benetel", "Isaac Hector", "Petra Collins",
+                "Amanda Mustard", "Olivia Bee", "Jimmy Nelsson"
+        };
+
+        public IEnumerable<string> Photographers 
+        { 
+            get
+            {
+                return from p
+                       in this.photographers 
+                       orderby p 
+                       select p;
+            }
+        }
 
         public void OnGet()
         {
             ViewData["Title"] = "Ofar Photography - Photographers";
-            Photographers = new[] 
-            { 
-                "Mary Ellen Mark", "Cindy Sherman", "Robert Frank", "Eliot Porter",
-                "Imogen Cunningham", "Frans Lanting", "Paul Strand", "Weegee",  
-                "Richard Avedon", "Don McCullin", "Edward Western", "Man Ray", 
-                "David Bailey",  "David LaChapelle", "Anne Geddes", "Dorothea Lange", 
-                "Robert Doisneau", "Steve McCurry", "Mario Testino", "André Kertész", 
-                "Andreas Gursky", "Robert Mapplethorpe", "Henri-Cartier Bresson", 
-                "Annie Leibovitz", "Sebastião Salgado", "Brassaï", "Ansel Adams", 
-                "Robert Capa" 
-            };
+
         }
     }
 }
