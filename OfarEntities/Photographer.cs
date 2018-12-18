3b567d6b9fe2878259c66d9448089c77252790d2
Ari Johannesson <arividar@gmail.com>
2018-12-18 00:25:56 +0000

    Renames and formats Photographer class file

---
+++ OfarEntities/Photographer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OfarEntities
+{
+    public class Photographer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Country { get; set; }
+    }
+}
