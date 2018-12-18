3e87848d64e06cadf6e5c89fac75e9e01e0a38b3
Ari Johannesson <arividar@gmail.com>
2018-12-18 00:20:46 +0000

    Adds Photographer entity class

--- OfarEntities/Class1.cs
+++ OfarEntities/Class1.cs
@@ -2,7 +2,12 @@
 
 namespace OfarEntities
 {
-    public class Class1
+    public class Photographer
     {
+        public long      Id         { get; set; }
+        public string    FirstName  { get; set; }
+        public string    LastName   { get; set; }
+        public DateTime? BirthDate  { get; set; }
+        public string    Country    { get; set; }
     }
 }
